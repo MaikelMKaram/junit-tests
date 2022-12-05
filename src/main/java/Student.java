0e963a6ca81e6c9c125e0a7536f149028b09fb2b
Maikel Karam <maikel.m.karam@gmail.com>
2022-12-05 13:47:11 -0600

    Junit progress save

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,16 @@
+import java.security.PrivateKey;
+import java.util.ArrayList;
+
 public class Student {
+
+    private Long id;
+    private String name;
+    private ArrayList<Integer> grades = new ArrayList<>();
+
+    public Student(Long id, String name, ArrayList<Integer> grades) {
+        this.id = id;
+        this.name = name;
+        this.grades = grades;
+    }
+
 }
